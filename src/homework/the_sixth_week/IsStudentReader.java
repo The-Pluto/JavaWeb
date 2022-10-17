8568225ecbd80bb6f94c530845bf66a32f14a523
liangcshao <2206884457@qq.com>
2022-10-17 14:13:32 +0800

    删除project_1外的文件

--- src/homework/the_sixth_week/IsStudentReader.java
+++
@@ -1,8 +0,0 @@
-package homework.the_sixth_week;
-
-import java.io.IOException;
-import java.util.ArrayList;
-
-public interface IsStudentReader {
-    public ArrayList<Student> read() throws IOException;
-}
