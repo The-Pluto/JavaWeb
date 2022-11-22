b47bae62f7ae0c1affb06e90c15962161eb92b2c
liangcshao <2206884457@qq.com>
2022-11-23 00:56:23 +0800

    删除src文件夹

--- src/project_1/the_sixth_week/IsStudentReader.java
+++
@@ -1,8 +0,0 @@
-package project_1.the_sixth_week;
-
-import java.io.IOException;
-import java.util.ArrayList;
-
-public interface IsStudentReader {
-    public ArrayList<Student> read() throws IOException;
-}
